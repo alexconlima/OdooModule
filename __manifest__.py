b0e68ecf6222d5624eb7c3ba87a4b86b6a8924ab
Alex <bes.alex@hotmail.es>
2024-11-22 18:25:41 +0100

    Manifest updated

--- __manifest__.py
+++ __manifest__.py
@@ -7,11 +7,8 @@
         Mòdul simple per gestionar l\'inventari.
     """,
     'author': 'Claudia Palacio, Marco Muñoz, Marc Sarrias, Alex Bes',
-    'depends': ['base'],
     'data': [
         'views/test.xml',
     ],
-    'installable': True,
-    'application': True,
     "website": "https://github.com/alexconlima/OdooModule",
 }
